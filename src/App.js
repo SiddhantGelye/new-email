2df914093b4047d9b1e46e3d27e7677c6b6b88ee
SiddhantGelye <siddhantwebdeveloper@gmail.com>
2021-06-29 20:37:59 +0530

    Email signature initial Update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import image from './siddhu.jpg'
+const App = ()=>{
+    return(
+        <div>
+            <div class="container">
+                <div class="email-signature">
+                    <div class="image">
+                        <img src={image} alt=""/>
+                    </div>
+                    <div class="content">
+                        <h1 class="name">Siddhant Gelye</h1>
+                        <h3 class="post">Full Stack Web Developer</h3>
+                        <ul class="contact">
+                            <li><i class="fas fa-phone-alt"></i> +919769545684</li>
+                            <li><i class="fas fa-envelope"></i>siddhantwebdeveloper@gmail.com</li>
+                            <li><i class="fas fa-map-marker-alt"></i> Wagle Estate, Thane(W)-400604</li>
+                        </ul>
+                        <ul class="icons">
+                            <li><i class="fab fa-facebook-f"></i></li>
+                            <li><i class="fab fa-twitter"></i></li>
+                            <li><i class="fab fa-instagram"></i></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    )
 }
-
 export default App;
