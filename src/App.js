2163c0ca632eaaada597653b7943d2232286fb5a
SiddhantGelye <siddhantwebdeveloper@gmail.com>
2021-07-03 19:11:18 +0530

    Sttle changed

--- src/App.js
+++ src/App.js
@@ -4,23 +4,23 @@ import image from './siddhu.jpg'
 const App = ()=>{
     return(
         <div>
-            <div class="container">
-                <div class="email-signature">
-                    <div class="image">
+            <div className="container">
+                <div className="email-signature">
+                    <div className="image">
                         <img src={image} alt=""/>
                     </div>
-                    <div class="content">
-                        <h1 class="name">Siddhant Gelye</h1>
-                        <h3 class="post">Full Stack Web Developer</h3>
-                        <ul class="contact">
-                            <li><i class="fas fa-phone-alt"></i> +919769545684</li>
-                            <li><i class="fas fa-envelope"></i>siddhantwebdeveloper@gmail.com</li>
-                            <li><i class="fas fa-map-marker-alt"></i> Wagle Estate, Thane(W)-400604</li>
+                    <div className="content">
+                        <h1 className="name">Siddhant Gelye</h1>
+                        <h3 className="post">Full Stack Web Developer</h3>
+                        <ul className="contact">
+                            <li><i className="fas fa-phone-alt"></i> +919769545684</li>
+                            <li><i className="fas fa-envelope"></i>siddhantwebdeveloper@gmail.com</li>
+                            <li><i className="fas fa-map-marker-alt"></i> Wagle Estate, Thane(W)-400604</li>
                         </ul>
-                        <ul class="icons">
-                            <li><i class="fab fa-facebook-f"></i></li>
-                            <li><i class="fab fa-twitter"></i></li>
-                            <li><i class="fab fa-instagram"></i></li>
+                        <ul className="icons">
+                            <li><i className="fab fa-facebook-f facebook"></i></li>
+                            <li><i className="fab fa-twitter twitter"></i></li>
+                            <li><i className="fab fa-instagram instagram"></i></li>
                         </ul>
                     </div>
                 </div>
